9b70841a29b77a095c3d329e169c403495fef859
pablolizardo <pablo_lizardo@yahoo.com.ar>
2013-09-09 19:05:10 -0300

    actualizados elementos de diseño

--- scripts/actors/element.js
+++ scripts/actors/element.js
@@ -21,7 +21,7 @@ define( [ 'data/context', 'scullge/actor' ], function( gaco, BaseActor )
 		var img = document.createElement( 'img' );
 		img.src = CONTEXT_PATH + '/images/items/' + this.properties.name + '.png';
 		img.style.left = ( $( '#cinta' ).offset().left + 40 ) + 'px';
-		img.style.left = '450px';
+		img.style.left = '440px';
 		img.style.top = '0px';
 		img.id = this.id;
 		img.className = 'Element';
@@ -38,17 +38,17 @@ define( [ 'data/context', 'scullge/actor' ], function( gaco, BaseActor )
 	{
 		var img = document.getElementById( this.id );
 		img.style.top = ( this.properties.top ) + 'px';
-		if( this.properties.top > 300 )
+		if( this.properties.top > 320 )
 		{
 			this.active = false;
 			gaco.gameVars.state = gaco.GameState.WAITING_ELEMENT;
-			$( img ).animate({ width: '70px' },
-				{
+			$( img ).animate({ width: '70px' , left: '472px',top:'370px' },
+			{
 					complete: function()
 					{
 						$( this ).attr( 'src', CONTEXT_PATH + '/images/sprites/explosion.gif' ).fadeOut();
 					},
-					duration: 200,
+					duration: 300, 
 			});
 		}
 	};
